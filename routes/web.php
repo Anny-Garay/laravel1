ea9b7fbe9f504030b445f2cbe03e11a13b4aa128
Anny-Garay <annyloyz@hotmail.com>
2022-11-02 21:08:40 +0100

    holis

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-//Llamamos a todos loscontroladores que usaremos
-use App\Http\Controllers\UsuariosController;
-use App\Http\Controllers\ProductoController;
-use App\Http\Controllers\AnnyController;
-use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\OficinaController;
-use App\Http\Controllers\PedidosController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,31 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/usuarios',function(){
-    return "Aqui van los usuarios";
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/usuarios2',
-    [UsuariosController::class,'listar']
-);
-
-Route::get('/productos',
-    [ProductoController::class,'listar']
-);
-
-Route::get('/anny',
-    [AnnyController::class,'listar']
-);
-Route::get('/clientes', 
-    [CustomerController::class, 'listar']
-);
-
-Route::get('/oficinas', 
-    [OficinaController::class, 'listar']
-);
-
-//Creamos una ruta para listar pedidos, en la parte de arriba ↑
-
-Route::get('/pedidos', 
-    [PedidosController::class, 'listar']
-);
+require __DIR__.'/auth.php';
